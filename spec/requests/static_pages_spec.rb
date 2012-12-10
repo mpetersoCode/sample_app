ccc658b92db1d0fcfb5d65e5b6bab2c284f0e0c9
Michael Peterson <Michael.Peterson@truestonefed.com>
2012-12-10 13:10:00 -0500

    Built Routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,19 +4,19 @@ describe "Statis pages" do
 
 	describe "Home page" do
 	
-		it "should have the content 'Sample App'" do
-		visit '/static_pages/home'
+		it "should have the h1 'Sample App'" do
+		visit root_path
 		page.should have_selector('h1', :text => 'Sample App')
 		end
 		
 		it "should have the right title" do
-		visit '/static_pages/home'
+		visit root_path
 		page.should have_selector('title',
 								:text => "Ruby on Rails Tutorial Sample App")
 		end
 		
 		it "should not have a custom page title" do
-		visit '/static_pages/home'
+		visit root_path
 		page.should_not have_selector('title', :text => '| Home')
 		end
 	end
@@ -24,12 +24,12 @@ describe "Statis pages" do
 	describe "Help page" do
 	
 		it "should have the content 'Help'" do
-		visit '/static_pages/help'
+		visit help_path
 		page.should have_selector('h1', :text => 'Help')
 		end
 		
 		it "should have the right title" do
-		visit '/static_pages/help'
+		visit help_path
 		page.should have_selector('title',
 								:text => "Ruby on Rails Tutorial Sample App | Help")
 		end
@@ -38,14 +38,28 @@ describe "Statis pages" do
 	describe "About page" do
 	
 		it "should have the content 'About Us'" do
-		visit '/static_pages/about'
+		visit about_path
 		page.should have_selector('h1', :text => 'About Us')
 		end
 		
 		it "should have the right title" do
-		visit '/static_pages/about'
+		visit about_path
 		page.should have_selector('title',
 								:text => "Ruby on Rails Tutorial Sample App | About")
+		end
+	end
+	
+	describe "Contact page" do 
+	
+		it "should have the h1 'Contact'" do
+			visit contact_path
+			page.should have_selector('h1', text: 'Contact')
+		end
+		
+		it "should have the title 'Contact'" do
+			visit contact_path
+			page.should have_selector('title',
+				text: "Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
