bf9244341e3b884303b1d295afd7e743962917a8
mpetersoCode <Michael.Peterson@truestonefed.com>
2012-12-13 07:06:38 -0500

    Continued work on chapter 8

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,10 +15,15 @@ describe "Authentication" do
 	before {visit signin_path }
 	
 	describe "with invalid information" do
-		before { click_button "sign in" }
+		before { click_button "Sign in" }
 	
 		it { should have_selector('title', text: 'Sign in') }
 		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+		
+		describe "after visiting another page" do
+		  before { click_link "Home" }
+		  it { should_not have_selector('div.alert.alert-error') }
+	  end
 	end
 	
 	describe "with valid information" do
