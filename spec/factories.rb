2d92dc3f0f3cbd04a8b993814f911f14b8465586
mpetersoCode <Michael.Peterson@truestonefed.com>
2012-12-16 22:30:18 -0500

    Continued work on CHapter 9, made it to 9.4 Deleting users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do
-		name "Michael Hartl"
-		email "michael@example.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "Person_#{n}@example.com"}
 		password "foobar"
-		password_confirmation  "foobar"
+		password_confirmation "foobar"
 	end
 end
